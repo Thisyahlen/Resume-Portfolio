23cb9612710dce012b19643b44d4334e4a94a96f
unknown <Thisyahlen>
2022-03-29 13:24:32 +0800

    notflix link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -64,7 +64,7 @@ const Work = () => {
                 Notflix{" "}
               </span>
               <div className="pt-8 text-center">
-                <a href="https://randompasswd.netlify.app/" target="_blank">
+                <a href="https://notflixmovies.netlify.app/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
